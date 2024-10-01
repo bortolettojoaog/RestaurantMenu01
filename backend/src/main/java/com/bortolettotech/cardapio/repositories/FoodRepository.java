00bdaf344b6517fe035a98cbae417995f0b16c8b
João G. Bortoletto <jgb3532@hotmail.com>
2024-10-01 20:20:17 -0300

    fix: add annotation

--- backend/src/main/java/com/bortolettotech/cardapio/repositories/FoodRepository.java
+++ backend/src/main/java/com/bortolettotech/cardapio/repositories/FoodRepository.java
@@ -2,8 +2,10 @@ package com.bortolettotech.cardapio.repositories;
 
 import com.bortolettotech.cardapio.entities.FoodEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.UUID;
 
+@Repository
 public interface FoodRepository extends JpaRepository<FoodEntity, UUID> {
 }
